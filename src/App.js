3d3e7624d5aa04473b88e8d0bbf1ce3ec76c3079
Lydia <lydiajarvistx@gmail.com>
2025-01-06 07:58:24 -0700

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './component/TodoList/TodoList';
+import NavbarMenu from './component/TodoList/NavbarMenu/NavbarMenu';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return ( 
     <>
       <h1>TODO LIST PROJECT</h1>
+      <NavbarMenu />
       <TodoList />
     </>
   );
---
+++ src/component/TodoList/NavbarMenu/NavbarMenu.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function NavbarMenu() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Contact Us</Nav.Link>
+        <Nav.Link href="#pricing">To Do List</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+  )
+}
