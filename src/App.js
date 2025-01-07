53c835a9412448d1b7274add7919a6cd01b16905
Lydia <lydiajarvistx@gmail.com>
2025-01-07 12:49:39 -0700

    adding components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TodoList from './component/TodoList/TodoList';
 import NavbarMenu from './component/TodoList/NavbarMenu/NavbarMenu';
+import TodoList from './component/TodoList/TodoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
       <Routes>
           <Route path="/home" element={<HomePage />} />
-          <Route path="/contact" element={<ContactUsPage />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
     </>
